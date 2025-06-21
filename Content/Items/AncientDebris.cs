7b59e60b6eec437d6154f217c9eaca30c76bbac8
Furret78 <68061718+furret78@users.noreply.github.com>
2025-06-21 13:03:50 +0200

    redundancy removal for ancient debris

--- Content/Items/AncientDebris.cs
+++ Content/Items/AncientDebris.cs
@@ -12,13 +12,7 @@ namespace BuriedHeavens.Content.Items {
         public override void SetDefaults() {
             Item.width = 32;
             Item.height = 30;
-            Item.value = 100;
-            Item.useTime = 24;
-            Item.useAnimation = 24;
-            Item.useStyle = ItemUseStyleID.Swing;
-            Item.UseSound = SoundID.Item1;
-            Item.consumable = true;
-            Item.noMelee = true;
+            Item.value = Item.buyPrice(silver: 1);
             Item.maxStack = Item.CommonMaxStack;
         }
 
